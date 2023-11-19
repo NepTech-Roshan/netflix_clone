7ad0bbb7f5cd157ee1bed9d49994dec0b7270e1f
Roshan Patel <79070796+NepTech-Roshan@users.noreply.github.com>
2023-11-19 19:57:11 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// const elements=document.querySelectorAll('.plus-1, .plus-3, .plus-6');
+// elements.forEach(element => {
+//     element.addEventListener('click',() => {
+//        const children = element.querySelectorAll(' ');
+//        children.foreach(child => {
+//         child.style.display = 'block';
+//       });
+//     
